a5c8cfd130422fbc5ee086a0f3726ece306a17fb
fsoc <8392143a@gmail.com>
2017-12-12 18:42:24 +0100

    Initial commit.

---
+++ routes/upload.js
@@ -0,0 +1,21 @@
+const id = require('shortid')
+
+const router = require('express').Router()
+
+module.exports = router
+
+const multer = require('multer')
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: 'files/',
+    filename: (req, f, cb) =>
+      cb(null,
+         id.generate() + f.originalname.slice(f.originalname.lastIndexOf('.')))
+  }),
+  limits: { fieldSize: 1024 * 1024 * 100 }
+})
+
+router.post('/', upload.array('files'), (req, res) =>
+  res.render('status', { files: req.files }))
+
+router.get('/', (req, res) => res.redirect('../'))
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.enable('trust proxy')
+
+app.use(require('helmet')())
+app.use(require('morgan')('dev', {
+  stream: { write: msg => require('./logger').verbose(msg.trim()) }
+}))
+
+app.use(express.static('public'))
+
+app.use('/upload', require('./routes/upload'))
+
+app.use(express.static('files'))
+
+app.listen(8084)
