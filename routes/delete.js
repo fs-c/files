0170174c94938b702c0e8d28e89addbd54774c68
fsoc <8392143a@gmail.com>
2017-12-14 16:09:30 +0100

    Expand /delete route.

--- routes/delete.js
+++ routes/delete.js
@@ -5,10 +5,23 @@ module.exports = router
 const fs = require('fs')
 const glob = require('glob')
 
-router.get('/:path', (req, res) => {
+router.get('/:path', (req, res, next) => {
+  const success = [  ], error = [  ]
+
   const files = glob.sync('./files/' + req.params.path) || [  ]
 
-  for (const file of files) try { fs.unlinkSync(file) } catch(e) {}
+  if (!files[0])
+    next(new Error('No file(s) match that query.'))
 
-  res.redirect('../')
+  for (const file of files) 
+    try { 
+      fs.unlinkSync(file)
+      success.push(file)
+    } catch(e) { error.push(file) }
+
+  res.render('status', {
+    status: 'ok',
+    message: `Deleted ${success.length} file(s)` + 
+      `${error ? `, failed deleting ${error.length} file(s)` : ''}.`
+  })
 })
