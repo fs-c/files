a01c70e0d77c48eb6fa000655a7e95468f4a1990
fsoc <8392143a@gmail.com>
2018-01-03 02:38:08 +0100

    Markdown support.

---
+++ routes/markdown.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+const marked = require('marked')
+
+const router = require('express').Router()
+
+module.exports = router
+
+router.get('/', (req, res, next) => {
+  let file = req.originalUrl
+  try { file = fs.readFileSync('./files' + file, 'utf8') } catch(e) { return next(e) }
+
+  res.render('markdown', { markdown: marked(file) })
+})
