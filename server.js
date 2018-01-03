f7a73b40bfec5b34f0e89fc7e8ce428ded243836
fsoc <8392143a@gmail.com>
2018-01-03 18:05:47 +0100

    Add comments.

--- server.js
+++ server.js
@@ -4,26 +4,35 @@ const app = express()
 app.set('view engine', 'ejs')
 app.enable('trust proxy')
 
+// Basic security and logging.
 app.use(require('helmet')())
 app.use(require('morgan')('dev', {
   stream: { write: msg => require('./logger').verbose(msg.trim()) }
 }))
 
+// Call cleanup at every request.
 app.use(require('./scripts/cleanup'))
 
+// Serve resources.
 app.use(express.static('public'))
 
+// Serve index page.
 app.use('/', require('./routes/index'))
 
+// Serve file index.
 app.use('/index', require('./routes/fileindex'))
 
+// API.
 app.use('/upload', require('./routes/upload'))
 app.use('/delete', require('./routes/delete'))
 
+// Render *.md files properly.
 app.use('/*.md', require('./routes/markdown'))
 
+// Serve files.
 app.use(express.static('files'))
 
+// Catch-all for errors.
 app.use(require('./routes/error'))
 
 app.listen(8084)
