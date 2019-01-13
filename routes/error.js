72802d285fa7d8f0046c0b13b0325ee6f84e69b2
lw <me@lw.ag>
2019-01-13 19:43:14 +0100

    Clear master branch

--- routes/error.js
+++
@@ -1,9 +0,0 @@
-const handle = (err, req, res, next) => {
-  res.status(500).render('status', {
-    status: 'error',
-    head: err.message.length < 100 ? err.message : '',
-    message: err.message.length > 100 ? err.message : ' '
-  })
-}
-
-module.exports = handle
--- routes/fileindex.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-
-module.exports = router
-
-router.get('/', (req, res) => 
-  res.render('fileindex', {
-    files: require('fs').readdirSync('./files')
-  }))
